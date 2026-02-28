a5ceeb339cd80442d4227d1e73b142d853165470
Mingtian Yang <mingtian07260@gmail.com>
2026-02-28 22:59:12 +0800

    Add GPLv3 compliance: source code links and license notices

--- src/about/About.js
+++ src/about/About.js
@@ -21,9 +21,11 @@ export function About(props) {
                 <img src={figure} className="figure-img" />
                 <p>由于不同软件对 iCalendar 日历的支持情况不同，生成的日历可能与校历有所偏差，请仔细核对后使用。</p>
                 <p>
-                    基于&nbsp;
+                    原版本由 @xmcp 开发，基于&nbsp;
                     <a href="https://www.gnu.org/licenses/gpl-3.0.zh-cn.html" target="_blank" rel="noopener noreferrer">GPLv3</a>
-                    &nbsp;协议在 <a href="https://github.com/xmcp/pku-syllabus" target="_blank" rel="noopener noreferrer">GitHub</a> 开源。
+                    &nbsp;协议在 <a href="https://github.com/pkuhelper-web/pku-syllabus" target="_blank" rel="noopener noreferrer">GitHub</a> 开源。
+                    本版本修复了选课表新增P/NP列导致的解析问题，源码见&nbsp;
+                    <a href="https://github.com/Morrow-Yang/pku-syllabus" target="_blank" rel="noopener noreferrer">此处</a>。
                 </p>
                 <p>
                     This program is distributed in the hope that it will be useful,
--- src/shared/Footer.js
+++ src/shared/Footer.js
@@ -6,12 +6,29 @@ const {Footer}=Layout;
 
 export function FooterComp(props) {
     return (
-        <Footer style={{textAlign: 'center', backgroundColor: 'transparent'}}>
-            {props.current_route===ROUTES.about ?
-                '课表助手' :
-                <a onClick={()=>{props.navigate(ROUTES.about);}}>关于课表助手</a>
-            }{' '}
-            by @xmcp
+        <Footer style={{textAlign: 'center', backgroundColor: 'transparent', fontSize: '12px', color: '#666'}}>
+            <div>
+                {props.current_route===ROUTES.about ?
+                    '课表助手' :
+                    <a onClick={()=>{props.navigate(ROUTES.about);}}>关于课表助手</a>
+                }{' '}
+                by @xmcp
+            </div>
+            <div style={{marginTop: '8px'}}>
+                <a href="https://github.com/Morrow-Yang/pku-syllabus" target="_blank" rel="noopener noreferrer">
+                    源代码
+                </a>
+                {' | '}
+                <a href="https://www.gnu.org/licenses/gpl-3.0.html" target="_blank" rel="noopener noreferrer">
+                    GPLv3 协议
+                </a>
+                {' | '}
+                基于{' '}
+                <a href="https://github.com/pkuhelper-web/pku-syllabus" target="_blank" rel="noopener noreferrer">
+                    原项目
+                </a>
+                {' '}修改
+            </div>
         </Footer>
     );
 }
